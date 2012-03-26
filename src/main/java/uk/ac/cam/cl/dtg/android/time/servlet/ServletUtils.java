6e430ab39fa1720d78519076666fcff74a075be8
Andrew Rice <acr31@cam.ac.uk>
2012-03-26 11:39:33 +0100

    Minor change: Refactored ServletUtils.getRequiredParameter to throw InvalidParamterException when a required value is missing rather than RequiredParameterException as before.

--- src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/ServletUtils.java
+++ src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/ServletUtils.java
@@ -14,11 +14,12 @@ import uk.ac.cam.cl.dtg.android.time.servlet.KeyManager.InvalidKeyException;
  * Class containing some useful service-wide stuff.
  * 
  * @author dt316
- *
+ * 
  */
 public class ServletUtils {
 
-  public static final String MIME_XML = "application/xml";
+	public static final String MIME_XML = "application/xml";
+
 	/**
 	 * Outputs an exception in a nice format.
 	 * 
@@ -26,21 +27,21 @@ public class ServletUtils {
 	 * @param e
 	 */
 	public static void PrintException(HttpServletResponse res, Exception e) {
-		
+
 		try {
 			PrintWriter out = res.getWriter();
-			
+
 			// Create an XML output factory
-			XMLOutputFactory factory = XMLOutputFactory.newInstance();			
-			XMLStreamWriter writer = factory.createXMLStreamWriter(out);			
-			
+			XMLOutputFactory factory = XMLOutputFactory.newInstance();
+			XMLStreamWriter writer = factory.createXMLStreamWriter(out);
+
 			StringWriter stringWriter = new StringWriter();
 			String stackTrace = null;
 			e.printStackTrace(new PrintWriter(stringWriter));
 
 			// get the stackTrace as String...
 			stackTrace = stringWriter.toString();
-			
+
 			// Write to the document
 			writer.writeStartDocument();
 			writer.writeStartElement("response");
@@ -49,80 +50,95 @@ public class ServletUtils {
 			writer.writeEndElement();
 			writer.writeEndElement();
 			writer.writeEndDocument();
-			
+
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 	}
 
 	/**
-	 * Get the parameter name from the request req and if it is null throw a RequiredParameterException
-	 * @param req is the HttpServletRequest object for this request
-	 * @param name of the HTTP request value 
-	 * @param validOps if provided will ensure that the returned String is one of these values
+	 * Get the parameter name from the request req and if it is null throw a
+	 * RequiredParameterException
+	 * 
+	 * @param req
+	 *            is the HttpServletRequest object for this request
+	 * @param name
+	 *            of the HTTP request value
+	 * @param validOps
+	 *            if provided will ensure that the returned String is one of
+	 *            these values
 	 * @return the parameter value, not null
-	 * @throws RequiredParameterException if the parameter is not specified
+	 * @throws RequiredParameterException
+	 *             if the parameter is not specified
+	 * @throws InvalidParameterException
 	 */
-	public static String getRequiredParameter(HttpServletRequest req, String name, String... validOpts) throws RequiredParameterException {
-	  String answer = req.getParameter(name);
-	  if (answer == null){
-	    throw new RequiredParameterException(name);
-	  }
-	  if (validOpts != null && validOpts.length != 0) {
-		  for(String s : validOpts) 
-			  if (answer.equals(s)) return s;
-	  		throw new RequiredParameterException(name);
-	  }
-	  else {
-		  return answer;
-	  }
+	public static String getRequiredParameter(HttpServletRequest req,
+			String name, String... validOpts)
+			throws BadParameterException {
+		String answer = req.getParameter(name);
+		if (answer == null) {
+			throw new RequiredParameterException(name);
+		}
+		if (validOpts != null && validOpts.length != 0) {
+			for (String s : validOpts)
+				if (answer.equals(s))
+					return s;
+			throw new InvalidParameterException(
+					"Provided parameter not in expected list");
+		} else {
+			return answer;
+		}
 	}
 
-	public static void checkKeyForServices(HttpServletRequest req) throws InvalidKeyException, RequiredParameterException {
-    KeyManager.isValidForServices(ServletUtils.getRequiredParameter(req,"key"));    
-  }
+	public static void checkKeyForServices(HttpServletRequest req)
+			throws InvalidKeyException, BadParameterException {
+		KeyManager.isValidForServices(ServletUtils.getRequiredParameter(req,
+				"key"));
+	}
 
-  public static int getIntParameter(HttpServletRequest req, String name, int defaultValue)
-      throws InvalidParameterException {
-    String value = req.getParameter(name);
-    if (name == null) {
-      return defaultValue;
-    } else {
-      try {
-        return Integer.parseInt(value);
-      } catch (NumberFormatException e) {
-        throw new InvalidParameterException("Expected parameter " + name
-            + " to be an integer but was: " + value);
-      }
-    }
-  }
+	public static int getIntParameter(HttpServletRequest req, String name,
+			int defaultValue) throws InvalidParameterException {
+		String value = req.getParameter(name);
+		if (name == null) {
+			return defaultValue;
+		} else {
+			try {
+				return Integer.parseInt(value);
+			} catch (NumberFormatException e) {
+				throw new InvalidParameterException("Expected parameter "
+						+ name + " to be an integer but was: " + value);
+			}
+		}
+	}
 
-  public static class BadParameterException extends Exception {
-    private static final long serialVersionUID = 1L;
+	public static class BadParameterException extends Exception {
+		private static final long serialVersionUID = 1L;
 
-    public BadParameterException(String message) {
-      super(message);
-    }
-  }
+		public BadParameterException(String message) {
+			super(message);
+		}
+	}
 
-  /**
-   * Thrown if a required parameter is not specified.
-   * @author drt24
-   *
-   */
-  public static class RequiredParameterException extends BadParameterException {
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Thrown if a required parameter is not specified.
+	 * 
+	 * @author drt24
+	 * 
+	 */
+	public static class RequiredParameterException extends
+			BadParameterException {
+		private static final long serialVersionUID = 1L;
 
-    public RequiredParameterException(String name){
-      super("Must supply "+ name +" parameter.");
-    }
-  }
+		public RequiredParameterException(String name) {
+			super("Must supply " + name + " parameter.");
+		}
+	}
 
-  public static class InvalidParameterException extends BadParameterException {
-    private static final long serialVersionUID = 1L;
+	public static class InvalidParameterException extends BadParameterException {
+		private static final long serialVersionUID = 1L;
 
-    public InvalidParameterException(String message) {
-      super(message);
-    }
-  }
+		public InvalidParameterException(String message) {
+			super(message);
+		}
+	}
 }
