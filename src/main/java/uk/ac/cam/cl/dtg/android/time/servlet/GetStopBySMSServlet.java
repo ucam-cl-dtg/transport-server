e386573a3a2c89c3b8b25bd8901cad547932c0a5
Daniel Thomas <drt24@cam.ac.uk>
2012-05-31 18:05:21 +0100

    Use the database rather than the council when looking up smscodes (naptancodes)
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/GetStopBySMSServlet.java
+++ src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/GetStopBySMSServlet.java
@@ -1,11 +1,12 @@
 package uk.ac.cam.cl.dtg.android.time.servlet;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.http.HttpServletRequest;
 
 import uk.ac.cam.cl.dtg.android.time.buses.BusStop;
-import uk.ac.cam.cl.dtg.android.time.data.CouncilDataSource;
 
 /**
  * Provides XML/RPC interface to bus stop position web service Not exposed as
@@ -14,11 +15,11 @@ import uk.ac.cam.cl.dtg.android.time.data.CouncilDataSource;
  * 
  * @author dt316
  * 
+ * smscodes are naptan_codes
  */
 public class GetStopBySMSServlet extends OmniBusServlet {
 
 	private static final long serialVersionUID = -4945691160872319999L;
-
 
 	@Override
 	protected void xmlGet(HttpServletRequest req, Connection db,
@@ -27,7 +28,22 @@ public class GetStopBySMSServlet extends OmniBusServlet {
 		ServletUtils.checkKeyForServices(req, db);
 		String smsCode = ServletUtils.getRequiredParameter(req, "smscode");
 
-		BusStop stop = CouncilDataSource.lookupStopBySMS(smsCode);
+		BusStop stop = null;
+		String sql = "SELECT available_stops.atco_code, available_stops.lat, available_stops.long, stop_name FROM available_stops, naptan_extended_stop_info WHERE available_stops.atco_code = naptan_extended_stop_info.atco_code AND naptan_extended_stop_info.naptan_code =?";
+		PreparedStatement ps = db.prepareStatement(sql);
+		try {
+		  ps.setString(1, smsCode);
+		  ResultSet rs = ps.executeQuery();
+		  if (rs.next()) {
+		    stop = new BusStop(rs.getString("stop_name"), Double.parseDouble(rs.getString("lat")), Double.parseDouble(rs.getString("long")), rs.getString("atco_code"));
+		  }
+		  rs.close();//on error closed by ps.close()
+		} finally {
+		  ps.close();
+		}
+		if (stop == null){
+		  throw new NoSuchStopException(smsCode);
+		}
 
 		writer.open("response");
 		writer.open("stop");
@@ -42,4 +58,13 @@ public class GetStopBySMSServlet extends OmniBusServlet {
   public String getServletInfo() {
 		return "GetStopByInfoServlet by David Tattersall";
 	}
+
+  private static class NoSuchStopException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public NoSuchStopException(String smsCode) {
+      super("No stop with ref: " + smsCode);
+    }
+
+  }
 }
