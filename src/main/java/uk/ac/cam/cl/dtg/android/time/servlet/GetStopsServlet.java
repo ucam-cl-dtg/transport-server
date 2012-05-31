52e1422aac1fcafa6c667e7ae0c2dd8dcb821d08
Daniel Thomas <drt24@cam.ac.uk>
2012-05-31 15:17:22 +0100

    Revert "Cache the output in GetStopsServlet rather than the data that generates the output"
    Unfortunately it is not possible to inject a pre-existing fragment of
    XML into an XMLStreamWriter. It all gets escaped.
    
    This reverts commit 9073d74ae937c9de7410240a1bc4e06ea1e8411b.

--- src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/GetStopsServlet.java
+++ src/main/java/uk/ac/cam/cl/dtg/android/time/servlet/GetStopsServlet.java
@@ -24,10 +24,10 @@ import uk.ac.cam.cl.dtg.android.time.data.CouncilDataSource;
 public class GetStopsServlet extends OmniBusServlet {
 
 	private static class Cached {
-		String stops;
+		List<BusStop> stops;
 		long stored;
 
-		public Cached(String stops, long stored) {
+		public Cached(List<BusStop> stops, long stored) {
 			this.stops = stops;
 			this.stored = stored;
 		}
@@ -39,7 +39,7 @@ public class GetStopsServlet extends OmniBusServlet {
 	private Map<String, Cached> cachedResults;
 
 	// How long before cached results expire and must be reloaded
-	public static final int CACHE_TIMEOUT = 1000 * 60 * 60 * 24 * 7;// one week
+	public static final int CACHE_TIMEOUT = 1000 * 60 * 60 * 48;
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
@@ -55,19 +55,30 @@ public class GetStopsServlet extends OmniBusServlet {
 		ServletUtils.checkKeyForServices(req, db);
 		String level = ServletUtils.getRequiredParameter(req, "level");
 
-		String stops = getCachedResult(level);
+		List<BusStop> stops = getCachedResult(level);
 
-		// TODO(drt24): work out how to just use the database here.
-		// We can't do this simply as we don't store level information in the database
-		// so we have to make some sort of potentially breaking change where we only
-		// return information for one of the levels and return all the information for that level.
+		// TODO: fix (this note left by David) I think the problem is that this
+		// does a fetch for all the cambs stops but no others. Also why do we
+		// need this at all? Don't we have the stops in the database already? acr31
 		if (stops == null) {
-			stops = cacheResult(level, CouncilDataSource.getBusStops(Integer.parseInt(level)));
+			stops = CouncilDataSource.getBusStops(Integer.parseInt(level));
+			cacheResult(level, stops);
 		}
-		writer.raw(stops);
+
+		writer.open("response");
+		writer.open("stops", "count", String.valueOf(stops.size()));
+		for (BusStop stop : stops) {
+			writer.open("stop");
+			writer.textElement("name", stop.getName());
+			writer.textElement("ref", stop.getStopRef());
+			writer.textElement("lat", String.valueOf(stop.getLatitude()));
+			writer.textElement("long", String.valueOf(stop.getLongitude()));
+			writer.close("stop");
+		}
+		writer.close("response");
 	}
 
-	private String getCachedResult(String key) {
+	private List<BusStop> getCachedResult(String key) {
 		Cached record = cachedResults.get(key);
 		if (record == null)
 			return null;
@@ -79,29 +90,17 @@ public class GetStopsServlet extends OmniBusServlet {
 		return record.stops;
 	}
 
-  /**
-   * Stores a cached copy of a lookup result
-   * 
-   * @param key Key uniquely identifying the lookup result
-   * @param data Result to cache
-   */
-  private String cacheResult(String key, List<BusStop> stops) {
-    XMLWriter writer = new XMLWriter();
-    writer.open("response");
-    writer.open("stops", "count", String.valueOf(stops.size()));
-    for (BusStop stop : stops) {
-      writer.open("stop");
-      writer.textElement("name", stop.getName());
-      writer.textElement("ref", stop.getStopRef());
-      writer.textElement("lat", String.valueOf(stop.getLatitude()));
-      writer.textElement("long", String.valueOf(stop.getLongitude()));
-      writer.close("stop");
-    }
-    writer.close("response");
-    String result = writer.toString();
-    cachedResults.put(key, new Cached(result, System.currentTimeMillis()));
-    return result;
-  }
+	/**
+	 * Stores a cached copy of a lookup result
+	 * 
+	 * @param key
+	 *            Key uniquely identifying the lookup result
+	 * @param data
+	 *            Result to cache
+	 */
+	private void cacheResult(String key, List<BusStop> data) {
+		cachedResults.put(key, new Cached(data, System.currentTimeMillis()));
+	}
 
 	@Override
   public String getServletInfo() {
